6085f623cc078aa1cec4da27861454eab52c3f1d
Faheel Farkshatov <failfarkshatov@gmail.com>
2021-01-07 07:34:00 +0300

    refactor config module, add database credentials

---
+++ config/database_config.py
@@ -0,0 +1,18 @@
+import os
+from sqlalchemy import create_engine
+
+from dotenv import load_dotenv
+dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
+if os.path.exists(dotenv_path):
+    load_dotenv(dotenv_path)
+
+DB_CREDENTIALS = {
+    'host': os.getenv('db_host'),
+    'database': os.getenv('db_name'),
+    'port': os.getenv('db_port'),
+    'user': os.getenv('db_username'),
+    'password': os.getenv('db_password'),
+}
+
+ENGINE_STR = "postgresql+psycopg2://{user}:{password}@{host}:{port}/{database}".format(**DB_CREDENTIALS)
+SQL_ENGINE = create_engine(ENGINE_STR)
